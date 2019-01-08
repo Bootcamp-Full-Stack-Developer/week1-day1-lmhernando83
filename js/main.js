3ec5230d22ae2ac76e16c2511ae0cc52e7e0c6f3
lmhernando83 <lmhernando83@gmail.com>
2019-01-08 17:40:03 +0100

    Limpieza de codigo de los ejercicios del modulo-0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function menu_open() {
+    document.getElementById("modal_open").style.display = "flex";
+    document.getElementById("button_menu").style.display = "none";
+}
+function menu_close() {
+    document.getElementById("modal_open").style.display = "none";
+    document.getElementById("button_menu").style.display = "flex";
+}
